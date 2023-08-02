6107e7233f497fcefdc67d1d96ce42aa80b13c21
Arybjan <arybjen92@gmail.com>
2023-08-02 20:42:46 +0600

    cteate users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(_("Имя"), max_length=50)
+    email = models.EmailField(_("Почта"), unique=True)
+    birthday_date = models.DateField(_("Год рождения"), auto_now=True)
+    genders = models.ForeignKey("users.Genders", on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(_("Время создания"), auto_now=True)
+
+
+class Genders(models.Model):
+    title = models.CharField(_("Пол"), max_length=20)
