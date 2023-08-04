3ff6140bb140b1acf815b0e028c4f1b767f1e38e
Arybjan <arybjen92@gmail.com>
2023-08-04 11:04:44 +0600

    connect urls

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,9 @@ router = DefaultRouter()
 urlpatterns = [
     path("add-product/", views.AddProductAPIView.as_view(), name="add-product"),
     path("list-product/", views.ListProductAPIView.as_view(), name="list-product"),
+    path(
+        "add-description/",
+        views.DecsriptionProductAPIView.as_view(),
+        name="add-description",
+    ),
 ]
-
